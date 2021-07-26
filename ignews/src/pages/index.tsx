2d91afc02eaf778d4c79d1ab75bdfaf4b9e818fb
unknown <gabriel.cscanes@hotmail.com>
2021-07-26 19:25:17 -0300

    Estilização da Home

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton/SubscribeButton';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
@@ -6,7 +8,23 @@ export default function Home() {
       <Head>
         <title>Home | igNews</title>
       </Head>
-      <h1>Home</h1>
+      
+      <main className={styles.container}>
+        <section className={styles.welcome}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about <br/>the <span>React</span> world
+          </h1>
+          <p>
+            Get acess to all the publications <br/>
+            <span>for $9.90 month</span>
+          </p>
+
+          <SubscribeButton/>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   )
 }
