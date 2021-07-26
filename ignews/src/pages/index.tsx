75c3cfbfd1f18aa7cc3c0040aa3c4d627a3b6430
unknown <gabriel.cscanes@hotmail.com>
2021-07-26 02:08:55 -0300

    Reset CSS e imagens da Home

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,7 +1,12 @@
-import style from '../styles/home.module.scss';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <h1 className={style.title}>Home</h1>
+    <>
+      <Head>
+        <title>Home | igNews</title>
+      </Head>
+      <h1>Home</h1>
+    </>
   )
 }
