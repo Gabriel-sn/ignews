77ba116baa9250ddc41c185310c2c4b5a029dd6e
unknown <gabriel.cscanes@hotmail.com>
2021-07-26 04:28:15 -0300

    SignInButton e finalização do Header

---
+++ ignews/src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import SingInButton from '../SingInButton/SingInButton';
+import styles from './header.module.scss';
+
+export function Header(){
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="ig.news" />
+                <nav>
+                    <a className={styles.active}>Home</a>
+                    <a>Posts</a>
+                </nav>
+
+                <SingInButton/>
+            </div>
+        </header>
+    )
+}
