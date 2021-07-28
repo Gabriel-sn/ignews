0d8bd9f2369d779795f465317a13d93ff3f3bd0e
unknown <gabriel.cscanes@hotmail.com>
2021-07-28 03:43:49 -0300

    Configuração do Stripe e SSG

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,8 +1,17 @@
 import Head from 'next/head';
+import { GetStaticProps } from 'next';
 import { SubscribeButton } from '../components/SubscribeButton/SubscribeButton';
 import styles from './home.module.scss';
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    productId: string;
+    amount: number;
+  }
+}
+
+export default function Home({product}: HomeProps) {
   return (
     <>
       <Head>
@@ -17,14 +26,35 @@ export default function Home() {
           </h1>
           <p>
             Get acess to all the publications <br/>
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton/>
+          <SubscribeButton productId = {product.productId}/>
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Coding" />
       </main>
     </>
   )
+}
+
+
+export const getStaticProps : GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JHcAOFFHFI5228pEkfXTtnD');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+
+  return {
+    props: {
+      product,
+    },
+
+    revalidate: 60 * 60 * 24 // 24 Horas
+  }
 }
