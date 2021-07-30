beda9d4abd3ce76d12b1f66c0e873e95c4a370e6
unknown <gabriel.cscanes@hotmail.com>
2021-07-30 02:53:02 -0300

    Autenticação via Github

--- ignews/src/components/SingInButton/SingInButton.tsx
+++ ignews/src/components/SingInButton/SingInButton.tsx
@@ -1,19 +1,30 @@
+import {signIn, signOut, useSession} from 'next-auth/client';
+
 import {FaGithub} from 'react-icons/fa';
 import {FiX} from 'react-icons/fi';
 import styles from './singInButton.module.scss';
 
 export default function SingInButton() {
-    const isUserLoggedIn = true;
+    const [session] = useSession();
 
-    return isUserLoggedIn ? (
-        <button type="button" className={styles.signInButton}>
+    console.log(session)
+
+    return session ? (
+        <button 
+            type="button"
+            className={styles.signInButton}
+            onClick={() => signOut()}
+        >
             <FaGithub color="#04D361"/>
-            Gabriel Nascimento
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon}/>
         </button>
-    ) :
-    (
-        <button type="button" className={styles.signInButton}>
+    ) : (
+        <button
+            type="button" 
+            className={styles.signInButton}
+            onClick={() => signIn('github')}
+        >
             <FaGithub color="#eba417"/>
             Sing in with Github
         </button>
---
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,30 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+import { query } from 'faunadb';
+
+import {fauna} from '../../../services/fauna';
+
+export default NextAuth({
+    providers: [
+        Providers.GitHub({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            scope: 'read:user'
+        }),
+    ],
+
+    callbacks: {
+        async signIn(user, account, profile) {
+            const { email } = user;
+            
+            await fauna.query(
+                query.Create(
+                    query.Collection('users'),
+                    {data: email}
+                )
+            )
+            return true
+        },
+    }
+});
