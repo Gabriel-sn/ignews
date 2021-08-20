268f995a6dacfdbaae31d6ae923e2218428193c7
unknown <gabriel.cscanes@hotmail.com>
2021-08-20 19:26:44 -0300

    Criação do preview dos posts

--- ignews/src/components/SubscribeButton/SubscribeButton.tsx
+++ ignews/src/components/SubscribeButton/SubscribeButton.tsx
@@ -2,6 +2,8 @@ import { signIn, useSession } from 'next-auth/client';
 import styles from './subscribeButton.module.scss';
 import { getStripeJs } from '../../services/stripe-js';
 import { api } from '../../services/api';
+import { redirect } from 'next/dist/next-server/server/api-utils';
+import { useRouter } from 'next/router';
 
 interface SubscribeButtonProps {
     productId: string;
@@ -9,11 +11,16 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({productId}: SubscribeButtonProps){
     const [session] = useSession();
-
+    const router = useRouter();
     async function handleSubscribe() {
         if(!session){
             signIn('github')
             return;
+        }
+
+        if(session.activeSubscription) {
+            router.push('/posts')
+            return
         }
 
         try{
